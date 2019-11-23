8e75fe59174273dfb404d74a3fcd8a77762195e1
cnguu <www@cnguu.cn>
2019-11-23 09:48:41 +0800

    Update: deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,71 +1,54 @@
 #!/usr/bin/env bash
 #
+# VuePress generic deployment script
 # VuePress 通用部署脚本
 #
+# Windows can't execute .sh file, need to install git client
 # Windows 无法执行 .sh 文件，需要安装 git 客户端
 #
 # Author: cnguu
 # Email: www@cnguu.cn
 #
 
+# Start
 # 开始
 set -e
 
+# Compile
 # 编译
-# package.json 中需要有这一句："build": "vuepress build docs"
 yarn build
 
+# Delete the dist_temp folder
 # 删除 dist_temp 文件夹
 rm -fr dist_temp
 
+# Copy the dist folder to the dist_temp folder
 # 复制 dist 文件夹到 dist_temp 文件夹
 cp -ir dist dist_temp
 
-# 复制 README.md 文件到 dist_temp 文件夹
-cp -i README.md dist_temp
-
+# Go to the dist_temp directory
 # 进入 dist_temp 目录
 cd dist_temp
 
+# Create a new CNAME file and write to the gleehub.com domain
 # 新建 CNAME 文件，并写入 gleehub.com 域名
 echo gleehub.com > CNAME
 
-# 初始化仓库
-git init
-
-# 添加
-git add -A
-
-# 提交
-git commit -m deploy
-
+# Forced push to the master branch of the cnguu.github.io repository
 # 强制推送到 cnguu.github.io 仓库的 master 分支
+git init
+git add -A
+git commit -m deploy
 git push -f git@github.com:cnguu/cnguu.github.io.git master
 
-# 多仓库部署开始 ------
-
-# 删除 CNAME
-#rm CNAME
-
-# 新建 CNAME 文件，并写入 www.gleehub.com 域名
-#echo www.gleehub.com > CNAME
-
-# 添加
-#git add -A
-
-# 提交
-#git commit -m deploy
-
-# 强制推送到 cnguu.coding.me 仓库的 master 分支
-#git push -f git@git.dev.tencent.com:cnguu/cnguu.coding.me.git master
-
-# 多仓库部署结束 ------
-
+# Return to the previous directory
 # 返回上一级目录
 cd ../
 
+# Delete the dist_temp folder
 # 删除 dist_temp 文件夹
 rm -fr dist_temp
 
+# End
 # 结束
 cd -
