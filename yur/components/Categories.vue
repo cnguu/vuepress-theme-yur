e8a0cf9aae97365927f94c7c49d438d6eec477be
cnguu <www@cnguu.cn>
2020-09-26 12:09:18 +0800

    feat: category posts

--- yur/components/Categories.vue
+++ yur/components/Categories.vue
@@ -1,9 +1,28 @@
 <template>
-  <div>
-    Categories
+  <div id="categories">
+    <posts :posts="posts" />
   </div>
 </template>
 
 <script>
-export default {};
+import Posts from "@theme/components/Posts";
+
+export default {
+  components: { Posts },
+  data() {
+    return {
+      posts: []
+    };
+  },
+  created() {
+    this.handleInit();
+  },
+  methods: {
+    handleInit() {
+      this.posts = this._postsByUpdated.filter(
+        post => post.id === this.$route.meta.id
+      );
+    }
+  }
+};
 </script>
