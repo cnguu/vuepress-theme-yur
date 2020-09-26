270ff99395d470c518b81aeca576c3c7054e45c7
cnguu <www@cnguu.cn>
2020-09-26 09:59:11 +0800

    feat: search posts

--- yur/components/Search.vue
+++ yur/components/Search.vue
@@ -23,8 +23,7 @@ export default {
       banner:
         "https://cdn.jsdelivr.net/gh/cnguu/pic@master/20171231/banners/6f6ca5b0cb3bc43e5895911cc40a343d.jpg",
       loading: false,
-      disabled: false,
-      posts: []
+      disabled: false
     };
   },
   created() {
@@ -41,15 +40,42 @@ export default {
       this.loading = true;
       this.disabled = true;
 
+      let posts = [];
+
       if (value) {
         value = value.trim().toLowerCase();
-        console.log(value);
-        console.log(this._postsByUpdated);
+
+        const matchTitle = item =>
+          item.title && item.title.toLowerCase().indexOf(value) > -1;
+        const matchHeader = item => {
+          return item.find(
+            header =>
+              header.title && header.title.toLowerCase().indexOf(value) > -1
+          );
+        };
+        const matchTag = item => {
+          return item.find(tag => tag.toLowerCase().indexOf(value) > -1);
+        };
+
+        for (let i = 0; i < this._postsByUpdated.length; i++) {
+          const p = this._postsByUpdated[i];
+
+          if (
+            matchTitle(p) ||
+            (p.headers && p.headers.length > 0 && matchHeader(p.headers)) ||
+            (p.frontmatter &&
+              p.frontmatter.tags &&
+              p.frontmatter.tags.length > 0 &&
+              matchTag(p.frontmatter.tags))
+          ) {
+            posts.push(p);
+          }
+        }
       } else {
-        this.posts = [];
+        posts = this._postsByUpdated;
       }
 
-      this.$emit("change", this.posts);
+      this.$emit("change", posts);
       this.loading = false;
       this.disabled = false;
     }
